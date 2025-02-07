b974d32b1dc499381caf7064eaa3879401b4a3e3
Михаил <mishhard@yandex.ru>
2025-02-07 20:56:43 +0300

    11 Почти готово

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,21 +1,44 @@
 package com.example.playlistmaker
 
+import TracksAdapter
 import android.content.Context
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
-import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.button.MaterialButton
-import android.view.inputmethod.InputMethodManager
 import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.internal.ViewUtils.hideKeyboard
+import retrofit2.*
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : BaseActivity() {
 
-    private var searchQuery: String = ""
+    private val itunesBaseUrl = "https://itunes.apple.com"
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(itunesBaseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val itunesService = retrofit.create(ItunesApi::class.java)
+
+    private lateinit var queryInput: EditText
+    private lateinit var placeholderMessage: TextView
+    private lateinit var placeholderContainer: View
+    private lateinit var placeholderImage: ImageView
+    private lateinit var tracksList: RecyclerView
+    private lateinit var clearButton: View
+    private lateinit var retryButton: Button
+
+    private val tracks = ArrayList<Track>()
+    private val adapter = TracksAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,73 +47,113 @@ class SearchActivity : BaseActivity() {
         val rootView = findViewById<LinearLayout>(R.id.root_view)
         applySystemBarsPadding(rootView)
 
-        val backButton: ImageView = findViewById(R.id.backButton)
-        backButton.setOnClickListener {
-            onBackPressedDispatcher.onBackPressed()
+        placeholderContainer = findViewById(R.id.placeholderContainer)
+        placeholderImage = findViewById(R.id.placeholderImage)
+        placeholderMessage = findViewById(R.id.placeholderMessage)
+        queryInput = findViewById(R.id.searchEditText)
+        tracksList = findViewById(R.id.rvTracks)
+        clearButton = findViewById(R.id.clearButton)
+        retryButton = findViewById(R.id.retryButton)
+
+        adapter.tracks = tracks
+        tracksList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        tracksList.adapter = adapter
+
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        queryInput.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                performSearch()
+                hideKeyboard()
+                true
+            } else {
+                false
+            }
         }
 
-        val searchEditText = findViewById<EditText>(R.id.searchEditText)
-        val clearButton = findViewById<MaterialButton>(R.id.clearButton)
 
         clearButton.setOnClickListener {
-            searchEditText.setText("")
-            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.hideSoftInputFromWindow(searchEditText.windowToken, 0)
+            queryInput.setText("")
+            clearButton.visibility = View.GONE
+            clearTracksList()
+        }
+        retryButton.setOnClickListener {
+            performSearch()
         }
 
-        if (savedInstanceState != null) {
-            searchQuery = savedInstanceState.getString("SEARCH_QUERY", "")
-            searchEditText.setText(searchQuery)
-            searchEditText.setSelection(searchQuery.length)
-        }
-
-        val simpleTextWatcher = object : TextWatcher {
+        queryInput.addTextChangedListener(object : android.text.TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                clearButton.visibility = clearButtonVisibility(s)
+                clearButton.visibility = if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
             }
 
-            override fun afterTextChanged(s: Editable?) {
-                searchQuery = s.toString()
+            override fun afterTextChanged(s: android.text.Editable?) {}
+        })
+    }
+
+    private fun performSearch() {
+        val query = queryInput.text.toString().trim()
+
+
+        itunesService.search(query).enqueue(object : Callback<SearchResponse> {
+            override fun onResponse(call: Call<SearchResponse>, response: Response<SearchResponse>) {
+                if (response.isSuccessful) {
+                    tracks.clear()
+                    response.body()?.results?.let {
+                        tracks.addAll(it)
+                    }
+                    adapter.notifyDataSetChanged()
+
+                    if (tracks.isEmpty()) {
+                        showMessage(getString(R.string.nothing_found), true, isError = false)
+                    } else {
+                        showMessage("", false)
+                    }
+                } else {
+                    showMessage(getString(R.string.connection_problem), true, isError = true)
+                }
             }
-        }
 
-        searchEditText.addTextChangedListener(simpleTextWatcher)
-
-        val rvTracks = findViewById<RecyclerView>(R.id.rvTracks)
-        val trackList = listOf(
-            Track("Smells Like Teen Spirit", "Nirvana", "5:01", "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"),
-            Track("Billie Jean", "Michael Jackson", "4:35", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"),
-            Track("Stayin' Alive", "Bee Gees", "4:10", "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"),
-            Track("Whole Lotta Love", "Led Zeppelin", "5:33", "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"),
-            Track("Sweet Child O'Mine", "Guns N' Roses", "5:03", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg")
-        )
-
-        val trackAdapter = TrackAdapter(trackList)
-        rvTracks.adapter = trackAdapter
-
-
+            override fun onFailure(call: Call<SearchResponse>, t: Throwable) {
+                showMessage(getString(R.string.connection_problem), true, isError = true)
+            }
+        })
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putString("SEARCH_QUERY", searchQuery)
-    }
 
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        searchQuery = savedInstanceState.getString("SEARCH_QUERY", "")
-        val searchEditText = findViewById<EditText>(R.id.searchEditText)
-        searchEditText.setText(searchQuery)
-        searchEditText.setSelection(searchQuery.length)
-    }
+    private fun showMessage(message: String, showPlaceholder: Boolean, isError: Boolean = false) {
+        if (showPlaceholder) {
+            placeholderContainer.visibility = View.VISIBLE
+            placeholderMessage.text = message
+            tracksList.visibility = View.GONE
 
-    private fun clearButtonVisibility(s: CharSequence?): Int {
-        return if (s.isNullOrEmpty()) {
-            View.GONE
+            if (isError) {
+                placeholderImage.setImageResource(R.drawable.track_connection_problem)
+                placeholderMessage.text = getString(R.string.connection_problem)
+                retryButton.visibility = View.VISIBLE
+            } else {
+                placeholderImage.setImageResource(R.drawable.track_nothing_found)
+                placeholderMessage.text = getString(R.string.nothing_found)
+                retryButton.visibility = View.GONE
+            }
         } else {
-            View.VISIBLE
+            placeholderContainer.visibility = View.GONE
+            tracksList.visibility = View.VISIBLE
+            retryButton.visibility = View.GONE
         }
+    }
+
+
+    private fun hideKeyboard() {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(queryInput.windowToken, 0)
+    }
+    private fun clearTracksList() {
+        tracks.clear()
+        adapter.notifyDataSetChanged()
+        showMessage("", false)
     }
 }
--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -1,8 +1,19 @@
 package com.example.playlistmaker
 
+import java.text.SimpleDateFormat
+import java.util.Locale
+
 data class Track(
     val trackName: String,
     val artistName: String,
-    val trackTime: String,
-    val artworkUrl100: String,
-    )
+    val trackTimeMillis: Long?,
+    val artworkUrl100: String
+) {
+    fun getFormattedTime(): String {
+        return if (trackTimeMillis != null) {
+            SimpleDateFormat("mm:ss", Locale.getDefault()).format(trackTimeMillis)
+        } else {
+            "00:00" // Возвращаем заглушку, если время не указано
+        }
+    }
+}
